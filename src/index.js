6345826413492dd6764b68fc580b197ce437d09b
aristakiss <mih.rodionov@gmail.com>
2022-01-10 01:25:20 +0400

    fix: fix index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    console.log(n);
+    n = Math.abs(n);
+    let tempN = (n < 100) ? '0' + n.toString() : n.toString();
+    let output = '';
+    for (let i = 2; i >= 0; i--) {
+      output += tempN[i]; 
+    }
+    return output;
 }
